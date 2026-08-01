33af0063ba0933bf4755e909f19c8ad38389d13f
Funmilayo <fummyoshe@gmail.com>
2026-08-01 15:21:53 -0400

    loop so user can replay or view scores w/o re-running

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 from quiz import run_quiz
 from score import display_top_5
-from utils import choose_game
+
+def exit_game():
+    print("Thank you for playing! Goodbye!")
+
 
 
 def main():
     print("Welcome, Welcome! What would you like to do today?\n")
+    choose_and_run_task()
+    while choose_and_run_task():
+        print("\nWhat would you like to do next?\n")
+        pass
+
+
+def choose_and_run_task():
 
     print("1. Start Quiz")
-    print("2. View Scores\n")
+    print("2. View Scores")
+    print("3. Exit\n")
 
-    user_response = input("Input 1 or 2: ")
-    while not (1 <= int(user_response) <= 2):
-        user_response = input("Please input a valid option between 1 and 2: ")
+    user_response = input("Input 1, 2, or 3: ")
+    while not (1 <= int(user_response) <= 3):
+        user_response = input("Please input a valid option between 1 and 3: ")
 
     if int(user_response) == 1:
         run_quiz()
     if int(user_response) == 2:
         display_top_5()
+    if int(user_response) == 3:
+        exit_game()
+        return False
+
+    return True
+
 
 if __name__ == "__main__":
     main()
